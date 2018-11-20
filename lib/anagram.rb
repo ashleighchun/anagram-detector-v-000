4c4b901f28f4baa932cb116438a28c76c9cef587
Ashleigh Chun <ashleigh.chun@wework.com>
2018-11-20 04:00:54 +0000

    automatically backed up by learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,2 +1,20 @@
-# Your code goes here!
-c
+require "pry"
+
+
+class Anagram
+
+@@matches = [] 
+attr_accessor :possible_anagrams 
+
+  def initialize(possible_anagrams)
+    @possible_anagrams = possible_anagrams
+    
+  end
+  
+  
+  
+  
+  
+end 
+
+  
