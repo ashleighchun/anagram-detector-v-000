c28003c2f510190dc6489fa66c7051f49c0d8a95
Ashleigh Chun <ashleigh.chun@wework.com>
2018-11-20 04:17:02 +0000

    automatically backed up by learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -12,9 +12,10 @@ class Anagram
   end
   binding.pry
   
-  def self.match(some_word)
-    #if some_word matches 
-    word_array = some_word.split("")
+  def self.match(given_array)
+    #if some_word matches any word(s) in given_array then return word(s)
+    #otherwise return empty array    
+    word_array = given_array.split(" ")
     word_array
   end
   @@matches
