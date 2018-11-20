e0795feab57d6f84ea8dd2cd672a41be6eb33f67
Ashleigh Chun <ashleigh.chun@wework.com>
2018-11-20 04:39:33 +0000

    automatically backed up by learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -17,7 +17,7 @@ class Anagram
     #if some_word matches any word(s) in given_array then return word(s)
     #otherwise return empty array    
     possible_anagram = some_word.split("")
-    given_array.each_slice("").to_a
+    x=given_array.each_slice("").to_a
     binding.pry
     if given_array.include?(possible_anagram)
       @@matches << some_word
